80ec0a671d67e9673b97bc5dff2759900ff19c13
David Ventura <davidsfsventura@gmail.com>
2024-01-21 01:53:48 +0000

    about and main

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,9 @@
     <link rel="icon" href="{{ asset(" assets/img/main_logo.png") }}">
     <link rel="stylesheet" href="{{ asset('assets/css/about.css') }}" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Diodav</title>
     @include('components.navbar')
     @include('components.backgroundwave')
@@ -23,6 +26,9 @@
                 <h4 style="font-weight: normal">David Ventura</h4>
                 <h4 style="font-weight: normal">19 years old</h4>
                 <h4 style="font-weight: normal">Portuguese</h4>
+                <a href="https://github.com/davidsfsventura/dventura-shop" target="_blank"><i
+                        class="fa-brands fa-github fa-2xl icon"></i></a>
+                <a href="/feedback" style="margin-left: 10px"><i class="fa-regular fa-envelope fa-2xl icon"></i></a>
             </div>
         </div>
         <div class="projectInfo">
@@ -35,7 +41,6 @@
             <h3>Project still in development</h3>
             </p>
         </div>
-        <a href="/feedback" class="feedback">Send feedback</a>
     </div>
 </body>
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,7 +17,6 @@
 <body>
     <h1 class="header">Project Website</h1>
     <a href="/shop" class="shop_around">Shop around</a>
-    <a href="https://github.com/davidsfsventura/dventura-shop" target="_blank" class="source_code">Source Code</a>
 </body>
 
 </html>
